bd6b5f3d66f8c12fd3a2e8d1804b5117bd0cca49
ligang33 <ligang33@baidu.com>
2023-03-21 11:28:07 +0800

    add other rank list of weibo

--- script/cronjob/hourly_download_hotsearch.py
+++ script/cronjob/hourly_download_hotsearch.py
@@ -18,26 +18,53 @@ from service.HotSearch import WeiboHotSearch
 BAIDU_HOT_SEARCH_URL = "https://top.baidu.com/board?tab=realtime"
 BAIDU_HOT_SEARCH_DATA_DIRECTORY = "/home/ligang/data/baidu_hot_search/"
 WEIBO_HOT_SEARCH_URL = "https://weibo.com/ajax/statuses/hot_band"
+
+WEIBO_SOCIAL_EVENT_URL = "https://s.weibo.com/top/summary?cate=socialevent"
+WEIBO_ENT_RANK_URL = "https://s.weibo.com/top/summary?cate=entrank"
+WEIBO_SPORT_URL = "https://s.weibo.com/top/summary?cate=sport"
+WEIBO_GAME_URL = "https://s.weibo.com/top/summary?cate=game"
+
 WEIBO_HOT_SEARCH_DATA_DIRECTORY = "/home/ligang/data/weibo_hot_search/"
+
+COOKIES_DIRECTORY = "/home/ligang/data/cookies/"
 
 
 def downloadBaiduHot():
     nowstr = datetime.now().strftime("%Y%m%d_%H%M%S")
-    fname = BAIDU_HOT_SEARCH_DATA_DIRECTORY + nowstr + '.json'
-    html = BaiduHotSearch.download_html(BAIDU_HOT_SEARCH_URL)
-    json_dict = BaiduHotSearch.parse_html(html)
+    fname_hotsearch = BAIDU_HOT_SEARCH_DATA_DIRECTORY + nowstr + '.json'
+    html_hotsearch = BaiduHotSearch.download_html(BAIDU_HOT_SEARCH_URL)
+    json_dict = BaiduHotSearch.parse_html(html_hotsearch)
 
-    with open(fname, "w") as f:
+    with open(fname_hotsearch, "w") as f:
         json.dump(json_dict, f, ensure_ascii=False)
 
 def downloadWeiboHot():
     nowstr = datetime.now().strftime("%Y%m%d_%H%M%S")
-    fname = WEIBO_HOT_SEARCH_DATA_DIRECTORY + nowstr + '.json'
-    html = WeiboHotSearch.download_html(WEIBO_HOT_SEARCH_URL)
-    json_dict = WeiboHotSearch.parse_html(html)
 
-    with open(fname, "w") as f:
+    # 下载热搜榜
+    fname_hotsearch = WEIBO_HOT_SEARCH_DATA_DIRECTORY + nowstr + '.json'
+    html_hotsearch = WeiboHotSearch.download_html(WEIBO_HOT_SEARCH_URL, cookies_dir=COOKIES_DIRECTORY)
+    json_dict = WeiboHotSearch.parse_html(html_hotsearch)
+
+    with open(fname_hotsearch, "w") as f:
         json.dump(json_dict, f, ensure_ascii=False)
+
+    # 下载要闻榜、文娱榜、体育榜、游戏榜
+    url_dict = {
+        "socialevent": WEIBO_SOCIAL_EVENT_URL,
+        "entrank": WEIBO_ENT_RANK_URL,
+        "sport": WEIBO_SPORT_URL,
+        "game": WEIBO_GAME_URL
+    }
+
+    for down_url, category in url_dict.items():
+        fname = BAIDU_HOT_SEARCH_DATA_DIRECTORY + nowstr + "_" + "category" + '.html'
+        html = WeiboHotSearch.download_html(down_url, cookies_dir=COOKIES_DIRECTORY)
+
+        with open(fname, "w") as f:
+            f.write(html)
+
+
 
 if __name__ == '__main__':
     print(__file__)
--- service/HotSearch/WeiboHotSearch.py
+++ service/HotSearch/WeiboHotSearch.py
@@ -11,12 +11,39 @@ import time
 import requests
 import simplejson as json
 
-def download_html(url):
+def download_html(url, cookies_dir="./"):
     html = ''
+    cookie_file = cookies_dir + "weibo.com.cookie"
+    weibo_cookie = ""
+
+    if os.path.exists(cookie_file):
+        with open(cookie_file, "r") as fr:
+            weibo_cookie = fr.read()
+    else:
+        weibo_cookie = \
+"SUBP=0033WrSXqPxfM72-Ws9jqgMF55529P9D9WW8HvYJSXpYZTGFsUaymCKa; \
+UOR=www.baidu.com,weibo.com,www.baidu.com; \
+SINAGLOBAL=48828572436.998566.1678416316360; \
+SUB=_2AkMTTaGdf8NxqwJRmPodzmzkaYx1wwvEieKlEVBGJRMxHRl-yT9vqnFYtRB6OM2PcgXSfJWpJIcDbmKZ0R12jkuaa1N1; \
+_s_tentry=www.baidu.com; \
+Apache=2406137559614.796.1679297740246; ULV=1679297740258:2:2:1:2406137559614.796.1679297740246:1678416316362"
+
+    req = requests.get(url)
+    headers = {
+        "cookie": weibo_cookie
+    }
+
     for i in range(3):
         try:
-            req = requests.get(url)
+            req = requests.get(url, headers=headers)
+            new_cookie = req.headers.get("Set-Cookie", "")
+            if not new_cookie:
+                new_cookie = req.headers.get("set-cookie", "")
+            if new_cookie:
+                with open(cookie_file, "w") as fw:
+                    fw.write(new_cookie)
             html = req.text
+
             if html:
                 break
         except Exception as e:
@@ -26,9 +53,14 @@ def download_html(url):
     return html
 
 
-def parse_html(html):
-    d = json.loads(html)
-    return d
+def parse_html(html, url=""):
+    if url == "https://weibo.com/ajax/statuses/hot_band":
+        d = json.loads(html)
+        return d
+    if url == "https://s.weibo.com/top/summary?cate=socialevent":
+        return {}
+
+    return {}
 
 def parse_json_dict(json_dict):
     data_list = []
@@ -66,15 +98,20 @@ def parse_json_dict(json_dict):
         print(item.get("hotScore", ""))
 
 
-
+def parse_html(html):
+    d = json.loads(html)
+    return d
 
 
 if __name__ == '__main__':
     print(__file__)
     print(os.getcwd() + '/../../')
 
-    html = download_html("https://weibo.com/ajax/statuses/hot_band")
-    print(html)
-    json_dict = parse_html(html)
-    print(json_dict)
+    #html = download_html("https://weibo.com/ajax/statuses/hot_band")
+    #print(html)
+    #json_dict = parse_html(html)
+    #print(json_dict)
     #parse_json_dict(json_dict)
+
+    html = download_html("https://s.weibo.com/top/summary?cate=socialevent")
+    print(html)
