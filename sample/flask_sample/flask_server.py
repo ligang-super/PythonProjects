533626f2036eaba7e6801da503a97957c6d30475
ligang33 <ligang33@baidu.com>
2023-04-17 11:24:18 +0800

    change flask sample

--- sample/flask_sample/flask_server.py
+++ sample/flask_sample/flask_server.py
@@ -12,13 +12,11 @@ import datetime
 import numpy as np
 import xml.etree.ElementTree as ET
 
-
 import flask
 import simplejson as json
 from flask import request
 import base64
 import io
-
 
 '''
 flask： web框架，通过flask提供的装饰器@server.route()将普通函数转换为服务
@@ -27,21 +25,31 @@ flask： web框架，通过flask提供的装饰器@server.route()将普通函数
 # 创建一个服务，把当前这个python文件当做一个服务
 
 
-
-
 server = flask.Flask(__name__)
 
-@server.route('/hello', methods=['GET'])
-def hello():
+
+@server.route('/hello1', methods=['GET'])
+def hello1():
     # 获取通过url请求传参的数据
-    print("hello")
+    print("hello1")
     k = request.args.get("k")
     v = request.args.get("v")
     print("k:", k, "v:", v)
     return flask.jsonify({'k': k, 'v': v}), 200
 
 
+@server.route('/hello2', methods=['GET'])
+def hello2():
+    # 获取通过url请求传参的数据
+    print("hello1")
+    k = request.args.get("k")
+    v = request.args.get("v")
+    print("k:", k, "v:", v)
+
+    r = {'code': '200', 'k': k, 'v': v}  # 返回数据
+
+    return json.dumps(r, ensure_ascii=False), 200
+
+
 if __name__ == '__main__':
     server.run(debug=True, port=8080, host='0.0.0.0')  # 指定端口、host设为0.0.0.0代表不管几个网卡，任何ip都可以访问
-
-
